a2ebeb150b27ce4e98b7649f5c1dd51450cc2093
whitedev <whitedev1902@gmail.com>
2025-07-14 11:17:52 -0400

    fix bug

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,10 +1,11 @@
 import React from "react";
+import ProfileData from "../data/profile";
 
 const Footer = () => {
   return (
     <div className="text-white bg-darkblue text-center py-4 mt-2 font-normal text-md md:text-lg md:p-5 md:mt-4">
       <p data-aos="zoom-in" data-aos-duration="1000" data-aos-once="false">
-        Made with ❤️ by <span className="font-bold"> Coby Rau</span>
+        Made with ❤️ by <span className="font-bold"> {ProfileData.name}</span>
       </p>
     </div>
   );
